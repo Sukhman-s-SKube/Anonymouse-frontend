082f140a8f60f92b9fbe83dc631a3ad1b4695562
aabusha3 <aabusha3@uwo.ca>
2025-02-12 15:51:53 -0500

    add person with keys for testing key exchange and other algorithms

--- src/encryption/logic/test.go
+++ src/encryption/logic/test.go
@@ -4,25 +4,69 @@ import (
 	"fmt"
 	"encoding/base64"
 	"encoding/hex"
-	"crypto/sha256"
+	"crypto/ecdh"
+	"crypto/rand"
 	"hash"
 	"io"
 	"golang.org/x/crypto/hkdf"
 )
 
 func main() {
-	secret := []byte{0x00, 0x01, 0x02, 0x03} // this should be the root key?
-	salt := []byte("salt") // this should be some shared constant?
-	info := []byte("hskdf example") // this should be the dh output?
+	// secret := []byte{0x00, 0x01, 0x02, 0x03} // this should be the root key?
+	// salt := []byte("salt") // this should be some shared constant?
+	// info := []byte("hskdf example") // this should be the dh output?
 
-	keys := hkdf_output(2, 32, sha256.New, secret, salt, info)
+	// Generate three 128-bit derived keys.
+	// keys := hkdf_output(2, 32, sha256.New, secret, nil, nil)
+	// fmt.Println(keys)
+	// fmt.Println()
+	
+	// for i := range keys {
+	// 	fmt.Println(keys[i]) //byte array
+	// 	fmt.Println(b16(keys[i])) //hex representation
+	// 	fmt.Println(b64(keys[i])) //base64 representation
+	// }
 
-	for i := range keys {
-		fmt.Println(keys[i]) //byte array
-		fmt.Println(b16(keys[i])) //hex representation
-		fmt.Println(b64(keys[i])) //base64 representation
-	}
+	alice := Person{name: "alice"}
+	alice.keyGen()
+	bob := Person{name: "bob"}
+	bob.keyGen()
 
+	fmt.Println(alice.name)
+	fmt.Println(alice.IK)
+	fmt.Println(alice.EK)
+	fmt.Println(alice.SPK)
+	fmt.Println(alice.OPK)
+
+	fmt.Println()
+	
+	fmt.Println(bob.name)
+	fmt.Println(bob.IK)
+	fmt.Println(bob.EK)
+	fmt.Println(bob.SPK)
+	fmt.Println(bob.OPK)
+	
+	fmt.Println()
+
+	fmt.Println("name:", alice.name == bob.name)
+	fmt.Println("IK:", alice.IK.Equal(bob.IK))
+	fmt.Println("EK:", alice.EK.Equal(bob.EK))
+	fmt.Println("SPK:", alice.SPK.Equal(bob.SPK))
+	fmt.Println("OPK:", alice.OPK.Equal(bob.OPK))
+	
+}
+
+type Person struct{
+	name string
+	IK, EK, SPK, OPK *ecdh.PrivateKey
+}
+
+func (person *Person) keyGen(){
+	curve := ecdh.X25519()
+	person.IK, _ = curve.GenerateKey(rand.Reader)
+	person.EK, _ = curve.GenerateKey(rand.Reader)
+	person.SPK, _ = curve.GenerateKey(rand.Reader)
+	person.OPK, _ = curve.GenerateKey(rand.Reader)
 }
 
 func hkdf_output (numKeys int, keySize int, hash func() hash.Hash, secret, salt, info []byte) [][]byte{
