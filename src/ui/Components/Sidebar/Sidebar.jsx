4fa847d7bc1be84f4040a1cca586f014b74f1e7c
Arshdeep Kehal <akehal@uwo.ca>
2025-02-11 19:25:18 -0500

    notifications

--- src/ui/Components/Sidebar/Sidebar.jsx
+++ src/ui/Components/Sidebar/Sidebar.jsx
@@ -4,20 +4,46 @@ import { toast } from "sonner";
 import { MdOutlineAddBox } from "react-icons/md";
 import { Button } from "@/Components/ui/button";
 
-export const Sidebar = ({ username, chatrooms, msgNotifs, currChatroom, setCurrChatroom, setAddNewChat }) => {
-    
+export const Sidebar = ({
+  username,
+  chatrooms,
+  msgNotifs,
+  currChatroom,
+  setCurrChatroom,
+  setAddNewChat,
+}) => {
+  return (
+    <div className="flex-auto max-w-80 bg-green-600 text-white p-5 box-border overflow-y-auto h-full shadow-[2px_0_5px_rgba(0,0,0,0.1)]">
+      <div className="flex justify-between">
+        <h3 className="mt-[0px] text-3xl font-bold">Chats</h3>
+        <MdOutlineAddBox
+          size={35}
+          className="cursor-pointer"
+          onClick={() => {
+            setAddNewChat(true);
+          }}
+        />
+      </div>
+      <div className="mt-[5px]">
+        {chatrooms == null || chatrooms.length == 0
+          ? 'No chatrooms to show'
+          : chatrooms.map((room) => (
+            <Button
+            variant={currChatroom === room ? "selected" : "inverse"}
+            className={`w-full my-[10px] ${msgNotifs[room._id] ? "font-bold" : ""}`}
+            key={room._id}
+            onClick={() => {
+                setCurrChatroom(room);
+                setMsgNotifs((prev) => {
+                return { ...prev, [room._id]: false };
+                });
+            }}
+            >
+            {room.name}
+            </Button>
 
-    return(
-        <div className="flex-auto max-w-80 bg-green-600 text-white p-5 box-border overflow-y-auto h-full shadow-[2px_0_5px_rgba(0,0,0,0.1)]">
-            <div className="flex justify-between">
-                <h3 className="mt-[0px] text-3xl font-bold">Chats</h3>
-                <MdOutlineAddBox size={35} className="cursor-pointer" onClick={() => {setAddNewChat(true)}}/>
-            </div>
-            <div className="mt-[5px]">
-                {chatrooms == null || chatrooms.length == 0 ? 'No chatrooms to show' : chatrooms.map((room) => (
-                    <Button variant={currChatroom == room ? "selected" : "inverse"} className={`w-full my-[10px] ${msgNotifs[room._id] ? 'font-bold bg-yellow-500' : ''}`} key={room._id} onClick={() => {setCurrChatroom(room)}}>{room.name}</Button>
-                ))}
-            </div>
-        </div>
-    );
+            ))}
+      </div>
+    </div>
+  );
 };
