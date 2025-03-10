b975b5dde87332b8c2e2bc3368d7659b76a29976
Ahmed El Ashmawy <aelashma@uwo.ca>
2025-03-10 14:46:02 -0400

    New DB schema and updated db functions accordingly

--- electron/main.js
+++ electron/main.js
@@ -3,8 +3,61 @@ import path from 'path';
 import isDev from 'electron-is-dev';
 import database from 'better-sqlite3-multiple-ciphers';
 import { createHash } from 'crypto';
+import fs from "fs";
 
 let mainWin
+
+const dbPath = (userId) => {
+    return path.join(isDev ? app.getAppPath() : app.getPath("userData"), userId + ".db");
+};
+
+const newDBQueries = (db) => {
+    let query = `
+        CREATE TABLE DHKey (
+            id INTEGER PRIMARY KEY,
+            public STRING NOT NULL,
+            private STRING NOT NULL
+            )`
+    ;
+    db.exec(query);
+    query = `
+        CREATE TABLE Person (
+            mongoId STRING PRIMARY KEY,
+            identity_priv_key STRING NOT NULL,
+            identity_pub_key STRING NOT NULL,
+            schnorr_priv_key STRING NOT NULL,
+            schnorr_pub_key STRING NOT NULL,
+            schnorr_sig STRING NOT NULL,
+            display_name STRING NOT NULL
+            )`
+    ;
+    db.exec(query);
+
+    query = `
+        CREATE TABLE Chatroom (
+            mongoId STRING PRIMARY KEY,
+            name STRING NOT NULL,
+            root_key STRING NOT NULL,
+            send_chain_key STRING,
+            receive_chain_key STRING,
+            self_priv_dh STRING,
+            other_pub_dh STRING
+            )`
+    ;
+    db.exec(query);
+
+    query = `
+        CREATE TABLE Message (
+            mongoId STRING PRIMARY KEY,
+            content STRING NOT NULL,
+            sender STRING NOT NULL,
+            chatroom_id STRING NOT NULL,
+            timestamp STRING NOT NULL,
+            FOREIGN KEY(chatroom_id) REFERENCES Chatroom(mongoId)
+            )`
+    ;
+    db.exec(query);
+};
 
 app.on('ready', () => {
     const win = new BrowserWindow({
@@ -29,149 +82,113 @@ app.on('activate', () => {
     if (BrowserWindow.getAllWindows().length === 0) mainWin = createWindow();
 });
 
-ipcMain.handle("createDB", async (event, args) => {
-    const db = new database(path.join(isDev ? app.getAppPath() : app.getPath("userData"), 'app.db'));
+ipcMain.handle("createDB", async (event, userId) => {
+    if (fs.existsSync(dbPath(userId))) {
+        fs.unlink(dbPath(userId), (err) => {
+            if (err) console.log(err);
+        });
+    }
+    const db = new database(dbPath(userId));
     db.pragma('journal_mode = WAL');
-    // db.pragma(`rekey='${args[0]}'`);
-    let query = `
-        CREATE TABLE dh_keys (
-            id INTEGER PRIMARY KEY,
-            privKey STRING NOT NULL,
-            pubKey STRING NOT NULL
-            )`
-    ;
-    db.exec(query);
-    console.log(query);
+    db.pragma('foreign_keys = 1');
 
-    query = `
-        CREATE TABLE messages (
-            id INTEGER PRIMARY KEY,
-            mongoId STRING NOT NULL,
-            chatroom STRING NOT NULL,
-            sender STRING NOT NULL,
-            content STRING NOT NULL,
-            timestamp STRING NOT NULL
-            )`
-    ;
-    db.exec(query);
-
+    newDBQueries(db);
     db.close();
 });
 
-// ipcMain.handle("createDHTable", async (event, args) => {
-//     const db = new database(path.join(isDev ? app.getAppPath() : app.getPath("userData"), 'app.db'));
-//     // db.pragma(`key='${args[0]}'`);
+ipcMain.handle("checkDB", async (event, userId) => {
+    if (!fs.existsSync(dbPath(userId))) {
+        const db = new database(dbPath(userId));
+        db.pragma('journal_mode = WAL');
+        db.pragma('foreign_keys = 1');
+    
+        newDBQueries(db);
+        db.close();
+        return false;
+    }
+    return true;
+});
 
-//     const query = `
-//         CREATE TABLE dh_keys (
-//             id INTEGER PRIMARY KEY,
-//             privKey STRING NOT NULL,
-//             pubKey STRING NOT NULL
-//             )`
-//     ;
-//     db.exec(query);
-//     db.close();
-
-// });
-
-// ipcMain.handle("createMsgsTable", async (event, args) => {
-//     const db = new database(path.join(isDev ? app.getAppPath() : app.getPath("userData"), 'app.db'));
-//     // db.pragma(`key='${args[0]}'`);
-
-//     const query = `
-//         CREATE TABLE messages (
-//             id INTEGER PRIMARY KEY,
-//             mongoId STRING NOT NULL,
-//             chatroom STRING NOT NULL,
-//             sender STRING NOT NULL,
-//             content STRING NOT NULL,
-//             timestamp STRING NOT NULL
-//             )`
-//     ;
-//     db.exec(query);
-//     db.close();
-
-// });
-
-ipcMain.handle("insertMsg", async (event, msg) => {
-    const db = new database(path.join(isDev ? app.getAppPath() : app.getPath("userData"), 'app.db'));
+ipcMain.handle("initPerson", async (event, person, userId) => {
+    const db = new database(dbPath(userId));
     // db.pragma(`key='${args[0]}'`);
 
-    const insertData = db.prepare("INSERT INTO messages (mongoId, chatroom, sender, content, timestamp) VALUES (?, ?, ?, ?, ?)").run(msg._id, msg.chatroom, msg.sender, msg.message.content, msg.message.timestamp);
+    const insertData = db.prepare("INSERT INTO Person (mongoId, identity_priv_key, identity_pub_key, schnorr_priv_key, schnorr_pub_key, schnorr_sig, display_name) VALUES (?, ?, ?, ?, ?, ?, ?)").run(
+        person._id,
+        person.ik,
+        person.IK,
+        person.sk,
+        person.SK,
+        person.sSig,
+        person.name
+    );
     db.close();
 });
 
-ipcMain.handle("getMsgs", async (event, chatroom) => {
-    const db = new database(path.join(isDev ? app.getAppPath() : app.getPath("userData"), 'app.db'));
+ipcMain.handle("insertMsg", async (event, msg, userId) => {
+    const db = new database(dbPath(userId));
     // db.pragma(`key='${args[0]}'`);
 
-    const msgs = db.prepare("SELECT * FROM messages WHERE chatroom = ? ORDER BY id asc").all(chatroom);
+    const insertData = db.prepare("INSERT INTO Message (mongoId, content, sender, chatroom_id, timestamp) VALUES (?, ?, ?, ?, ?)").run(msg._id, msg.message.content, msg.sender, msg.chatroom, msg.message.timestamp);
+    db.close();
+});
+
+ipcMain.handle("getMsgs", async (event, chatroom, userId) => {
+    const db = new database(dbPath(userId));
+    // db.pragma(`key='${args[0]}'`);
+
+    const msgs = db.prepare("SELECT * FROM Message WHERE chatroom_id = ? ORDER BY timestamp asc").all(chatroom);
     db.close();
     
     return msgs;
 });
 
-ipcMain.handle("getMsg", async (event, msgId) => {
-    const db = new database(path.join(isDev ? app.getAppPath() : app.getPath("userData"), 'app.db'));
+ipcMain.handle("getMsg", async (event, msgId, userId) => {
+    const db = new database(dbPath(userId));
     // db.pragma(`key='${args[0]}'`);
 
-    const msg = db.prepare("SELECT * FROM messages WHERE mongoId = ?").get(msgId);
+    const msg = db.prepare("SELECT * FROM Message WHERE mongoId = ?").get(msgId);
     db.close();
     
     return msg;
 });
 
-ipcMain.handle("insertDHKeys", async (event, args) => {
-    const db = new database(path.join(isDev ? app.getAppPath() : app.getPath("userData"), 'app.db'));
+ipcMain.handle("insertDHKeys", async (event, keys, userId) => {
+    const db = new database(dbPath(userId));
     // db.pragma(`key='${args[0]}'`);
-    const keys = args;
 
     for (let key of keys) {
-        const insertData = db.prepare("INSERT INTO dh_keys (privKey, pubKey) VALUES (?, ?)").run(key.privKey, key.pubKey);
+        const insertData = db.prepare("INSERT INTO DHKey (private, public) VALUES (?, ?)").run(key.privKey, key.pubKey);
     }
     db.close();
 });
 
-ipcMain.handle("getKeys", async (event, numKeys) => {
-    const db = new database(path.join(isDev ? app.getAppPath() : app.getPath("userData"), 'app.db'));
+ipcMain.handle("getKeys", async (event, numKeys, userId) => {
+    const db = new database(dbPath(userId));
     // db.pragma(`key='${args[0]}'`);
 
-    const keys = db.prepare("SELECT id, pubKey FROM dh_keys ORDER BY id DESC LIMIT ?").all(numKeys);
+    const keys = db.prepare("SELECT id, public FROM DHKey ORDER BY id DESC LIMIT ?").all(numKeys);
     db.close();
     
     return keys;
 });
 
-ipcMain.handle("getDHKey", async (event, args) => {
-    const db = new database(path.join(isDev ? app.getAppPath() : app.getPath("userData"), 'app.db'));
+ipcMain.handle("getDHKey", async (event, keyId, userId) => {
+    const db = new database(dbPath(userId));
     // db.pragma(`key='${args[0]}'`);
-    const row = db.prepare("SELECT * FROM dh_keys WHERE id = ?").get(args);
+
+    const row = db.prepare("SELECT * FROM DHKey WHERE id = ?").get(keyId);
     db.close();
+
     return row
 });
 
-ipcMain.handle("delDHKey", async (event, args) => {
-    const db = new database(path.join(isDev ? app.getAppPath() : app.getPath("userData"), 'app.db'));
+ipcMain.handle("delDHKey", async (event, keyId, userId) => {
+    const db = new database(dbPath(userId));
     // db.pragma(`key='${args[0]}'`);
-    const delData = db.prepare("DELETE FROM dh_keys WHERE id = ?").run(args);
-    db.close();
-});
 
-ipcMain.handle("delAllDHKeys", async (event, args) => {
-    const db = new database(path.join(isDev ? app.getAppPath() : app.getPath("userData"), 'app.db'));
-    // db.pragma(`key='${args[0]}'`);
-    const delData = db.prepare("DELETE FROM dh_keys").run();
+    const delData = db.prepare("DELETE FROM DHKey WHERE id = ?").run(keyId);
     db.close();
-});
-
-ipcMain.on("alert", (event, args) => {
-    const options = {
-        type: "none",
-        buttons: ["Okay"],
-        title: "Alert Message!",
-        message: args,
-    }
-    dialog.showMessageBox(mainWin, options)
 });
 
 ipcMain.handle("sha256", async (event, str) => {
